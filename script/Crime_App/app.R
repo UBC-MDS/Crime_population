12b2039eb09cd15588ccedb07cf1f27aa5e2a380
keray3c1 <ckera.yucel@gmail.com>
2019-01-14 15:53:15 -0800

    add line plot _not working yet_ DEBUG

--- script/Crime_App/app.R
+++ script/Crime_App/app.R
@@ -1,3 +1,14 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
 library(shiny)
 library(tidyverse)
 library(ggplot2)
@@ -13,50 +24,62 @@ library(shiny)
 dat <- dat %>% filter(months_reported == 12)
 sum(is.na(dat))
 
+cities <- dat$department_name
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Crime Data Visualizer (U.S.)",
-              windowTitle = "US Crime App"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("year_input",
-                     "SELECT YEAR",
-                     min = 1975,
-                     max = 2015, 
-                     value = c(1975, 2015),
-                     sep = "")
-      ),
-      
-      # Show a plot of the generated distribution
+  
+  # Application title
+  titlePanel("Crime Data Visualizer (U.S.)",
+             windowTitle = "US Crime App"),
+  
+  # Sidebar select year range 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("year_input",
+                  "SELECT YEAR",
+                  min = 1975,
+                  max = 2015, 
+                  value = c(1975, 2015),
+                  sep = ""),
+      selectInput("department_name", "SELECT CITY",
+                  choices = cities,
+                  selected = "Atlanta"),
+    # select crime type
+      radioButtons("crime_type", 
+                   "SELECT CRIME TYPE",
+                   c("Homicide" = "homs_per_100k",
+                     "Rape" = "rape_per_100k",
+                     "Robbery" = "rob_per_100k",
+                     "Aggravated Assault" = "agg_ass_per_100k"),
+                   selected = "rape_per_100k")),
+ 
       mainPanel(
-        plotOutput("year_line"),   #COMMA HERE
-        tableOutput("table")
-      )
-   )
+        plotOutput("plot_crime")   
+    )
 )
+)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-                crime_filtered <- reactive({
-                  dat %>% 
-                    filter(year > input$year_input[1], 
-                           year < input$year_input[2])
-                })
-                
-                output$year_line <- renderPlot(
-                  crime_filtered() %>% 
-                    ggplot(aes(year)) +
-                    geom_line()
-                )                              #NO COMMA HERE
-                #output$table <- renderTable({
-                 # crime_filtered()
-                #})
+ crime_filtered <- reactive({
+   dat %>% 
+     filter(year > input$year_input[1], 
+            year < input$year_input[2])
+    
+    output$plot_crime <- renderPlot({
+      ggplot(crime_filtered, aes_(year_input, crime_type), department_name) +
+        geom_line()+
+        labs(x = "Crime per 100k", y = "Year")
+    }
+      
+    )
+    
+ }
+ )
 }
-
+ 
 # Run the application 
 shinyApp(ui = ui, server = server)
 
