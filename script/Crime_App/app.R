b69d3c66310d6a9fe84b5324b3009b7fbe8486aa
keray3c1 <ckera.yucel@gmail.com>
2019-01-14 19:45:28 -0800

    app not working- debugging required

--- script/Crime_App/app.R
+++ script/Crime_App/app.R
@@ -8,8 +8,6 @@
 #
 
 library(shiny)
-
-library(shiny)
 library(tidyverse)
 library(ggplot2)
 
@@ -21,10 +19,26 @@ str(dat)
 library(shiny)
 
 #filter out the data that was reported less than 12 months
-dat <- dat %>% filter(months_reported == 12)
-sum(is.na(dat))
 
-cities <- dat$department_name
+
+tidy_data <- (dat %>% 
+    gather( key = "crime_type", value = "count_per_100k", 
+            violent_per_100k, 
+            homs_per_100k, 
+            rape_per_100k, 
+            rob_per_100k, 
+            agg_ass_per_100k))
+
+
+dat <- tidy_data %>% filter(months_reported == 12)
+sum(is.na(tidy_data))
+
+cities <- unique(tidy_data$department_name)
+cities
+years <- unique(tidy_data$year)
+years
+crime <- unique(tidy_data$crime_type)
+crime
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -36,50 +50,81 @@ ui <- fluidPage(
   # Sidebar select year range 
   sidebarLayout(
     sidebarPanel(
-      sliderInput("year_input",
-                  "SELECT YEAR",
-                  min = 1975,
-                  max = 2015, 
-                  value = c(1975, 2015),
-                  sep = ""),
       selectInput("department_name", "SELECT CITY",
                   choices = cities,
                   selected = "Atlanta"),
+      br(),
+      
+      sliderInput("year_input", "Year",
+                  min = 1975, max = 2015, 
+                  value = c(1975, 2015), 
+                  sep = ""),
+      br(),
+      
     # select crime type
-      radioButtons("crime_type", 
-                   "SELECT CRIME TYPE",
-                   c("Homicide" = "homs_per_100k",
-                     "Rape" = "rape_per_100k",
-                     "Robbery" = "rob_per_100k",
-                     "Aggravated Assault" = "agg_ass_per_100k"),
-                   selected = "rape_per_100k")),
+    radioButtons("crime_type", 
+                 "SELECT CRIME TYPE",
+                 choices = crime,
+                 selected = "rape_per_100k"),
  
-      mainPanel(
-        plotOutput("plot_crime")   
-    )
+      mainPanel(plotOutput("plot_crime"))
+)
 )
 )
 
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
- crime_filtered <- reactive({
-   dat %>% 
-     filter(year > input$year_input[1], 
-            year < input$year_input[2])
-    
-    output$plot_crime <- renderPlot({
-      ggplot(crime_filtered, aes_(year_input, crime_type), department_name) +
-        geom_line()+
-        labs(x = "Crime per 100k", y = "Year")
-    }
-      
-    )
-    
+ server <- function(input, output) {
+   crime_filtered <- reactive(
+     dat %>% filter(cities ==  input$department_name,
+                    crime_type ==  input$crime_type,
+                    year >= input$year_input[1] &
+                      year <= input$year_input[2])
+   )
+   
+   output$plot_crime <- renderPlot(
+     crime_filtered() %>% 
+       ggplot(aes_(input$year_input,
+                   input$crime_type, 
+                   input$department_name)) +
+       geom_line()+
+       labs(x = "Year", y = "Crime per 100k")
+   )
  }
- )
-}
+
+
+    
+##    if (input$crime_input == "homs_per_100k") {
+##      ggplot(filter_crime, aes(year, homs_per_100k, department_name)) +
+##        geom_line()+
+##        labs(x = "Year", y = "Homicide per 100K")+
+##        geom_point()
+##    }
+##    else if(input$crime_input == "rape_per_100k"){
+##      ggplot(filter_crime, aes(year, rape_per_100k, department_name)) +
+##        geom_line()+
+##        labs(x = "Year", y = "Rape per 100K")+
+##        geom_point()
+##    }
+##    else if(input$crime_input == "rob_per_100k"){
+##      ggplot(filter_crime, aes(year, rob_per_100k, department_name)) +
+##        geom_line()+
+##        labs(x = "Year", y = "Robbery per 100K")+
+##        geom_point()
+##    }
+##    else if(input$crime_input == "agg_ass_per_100k"){
+##      ggplot(filter_crime, aes(year, agg_ass_per_100k, department_name)) +
+##        geom_line()+
+##        labs(x = "Year", y = "Assaults per 100K")+
+##        geom_point()
+##    }
+##  
+##})
+##}
+      
  
+
+
 # Run the application 
 shinyApp(ui = ui, server = server)
 
